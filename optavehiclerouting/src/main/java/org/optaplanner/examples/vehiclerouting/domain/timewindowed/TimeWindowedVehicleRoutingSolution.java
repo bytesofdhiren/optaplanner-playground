cfd0015507fab76e41a297e5b60c78c3142073c2
dhiren <dhiren.patel@routerabbit.com>
2016-06-07 15:37:47 +0530

    updated

--- optavehiclerouting/src/main/java/org/optaplanner/examples/vehiclerouting/domain/timewindowed/TimeWindowedVehicleRoutingSolution.java
+++ optavehiclerouting/src/main/java/org/optaplanner/examples/vehiclerouting/domain/timewindowed/TimeWindowedVehicleRoutingSolution.java
@@ -16,10 +16,9 @@
 
 package org.optaplanner.examples.vehiclerouting.domain.timewindowed;
 
-import com.thoughtworks.xstream.annotations.XStreamAlias;
 import org.optaplanner.examples.vehiclerouting.domain.VehicleRoutingSolution;
 
-@XStreamAlias("VrpTimeWindowedVehicleRoutingSolution")
-public class TimeWindowedVehicleRoutingSolution extends VehicleRoutingSolution {
 
-}
+//public class TimeWindowedVehicleRoutingSolution extends VehicleRoutingSolution {
+//
+//}
